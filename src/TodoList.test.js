b562f6215b750bf343a98d26dd66933ab9738312
Malini Chandran <malinichandran@Malinis-MacBook-Air.local>
2021-07-06 16:33:34 -0700

    todo-list inital check in

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react';
 import TodoList from "./TodoList";
 
 function addTodo(todoList, task="Finish task"){
-    const taskInput = todoList.getByLabelText("task");
+    const taskInput = todoList.getByLabelText("Task to be added!");
     fireEvent.change(taskInput, {target:{value:task}});
     const button = todoList.getByText("Add a new task!");
     fireEvent.click(button);
@@ -21,5 +21,21 @@ it("matches snapshot", function(){
 //no todos added
 it("can add a new todo", function(){
     const todoList = render(<TodoList/>);
-    expect(t)
+    expect(todoList.queryByText("X")).not.toBeInTheDocument();
+    addTodo(todoList);
+
+//add a todo
+const removeButton = todoList.getByText("X");
+expect(removeButton).toBeInTheDocument();
+
+
+});
+
+it("can remove a box", function(){
+    const todoList = render(<TodoList/>);
+    addTodo(todoList);
+
+    const removeButton = todoList.getByText("X");
+    fireEvent.click(removeButton);
+    
 })
